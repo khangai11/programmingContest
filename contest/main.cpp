2af9f76247f8709df2e3226ba64cf47d32b7ee20
khangai <khangai@t-ksj.co.jp>
2021-08-02 19:01:35 +0900

    D - Integers Have Friends time limit exceeded on test 3

--- contest/main.cpp
+++ contest/main.cpp
@@ -15,8 +15,8 @@ using namespace std;
 #define ll long long
 #define INF ((1<<30)-1)
 #define MOD 1000000007
-#define rep(a,b) for(int a=0;a<b;a++)
-#define rrep(a,b) for(int a=1;a<=b;a++)
+#define rep(a,b) for(int a=0;a<b;++a)
+#define rrep(a,b) for(int a=1;a<=b;++a)
 
 vector<ll> primes;
 ll primemax = 2;
@@ -24,53 +24,66 @@ ll num = 1;
 
 bool checkPrime(ll n) {
 	bool ret = true;
-	size_t s = primes.size();
-	for (ll k = primemax + 1; k <= n; k++) {
+	if (primemax > n)
+		return true;
 
-	}
-	for (size_t i = 0; i < primes.size(); i++) {
-		if (v % primes[i] == 0) {
-			ret = false;
-			break;
+	for (ll k = primemax + 1; k <= n; k++) {
+		if (k % 2 == 0)
+			continue;
+
+		ret = true;
+		for (ll i = 0; i < num && primes[i]<k; i++) {
+			if (k % primes[i] == 0) {
+				ret = false;
+				break;
+			}
+		}
+		if (ret) {
+			primes.push_back(k);
+			num++;
 		}
 	}
-	if (ret) {
-		primes.push_back(v);
-	}
+	primemax = n;
 	return ret;
 }
 
 void solveTest() {
 	int n; cin >> n;
-	int val = 0;
+	int val = 1;
 	long long mi = 1100100100100100100, ma = 0;
+	ll max_dif;
 	vector<long long> a(n + 1);
 	rep(i, n) {
 		cin >> a[i];
 		mi = min(mi, a[i]);
 		ma = max(ma, a[i]);
 	}
-	ll max_div = 0;
-	val = (n + 1) / 2;
-	max_div = (ma - mi) / val;
-	for (ll d = 3; d <= max_div; d++) {
-		if (isPrime(d)) {
-			vector<ll> rem(d, 0);
-			ll maxva = 0;
-			for (int i = 0; i < n;i++) {
-				ll t = a[i] % d;
-				rem[t] ++;
-				maxva = max(maxva, rem[t]);
+	max_dif = ma - mi;
+	checkPrime(max_dif);
+	for (ll i = 0;max_dif >= primes[i] * val && i<num;++i) {
+		ll div = primes[i];
+		ll remain = a[0]%div;
+		int cont = 1;
+		for (int j = 1;j < n;j++) {
+			if (remain == (a[j] % div)) {
+				cont++;
 			}
-			val = max((ll)val, maxva);
+			else {
+				val = max(val, cont);
+				remain = (a[j] % div);
+				cont = 1;
+			}
 		}
+		val = max(val, cont);
+		if (val == n)
+			break;
 	}
 	cout << val << "\n";
 }
 
 int main() {
 	int test; cin >> test;
-	primes[0] = 2;
+	primes.push_back(2);
 	while (test--)
 		solveTest();
 }

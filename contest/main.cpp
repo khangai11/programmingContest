f37f4c25ff21fcfdfb4968c99782053ffbed6786
khangai <khangai@t-ksj.co.jp>
2021-08-19 17:26:03 +0900

    https://codeforces.com/contest/1560/problem/F1

--- contest/main.cpp
+++ contest/main.cpp
@@ -140,7 +140,7 @@ public:
 		left = ind * 2 + 1;
 		right = left + 1;
 		if (ind >= n - 1) {
-			return (ind-n+1);
+			return (ind - n + 1);
 		}
 		//if (v[ind] < sum)
 		//	return 0;
@@ -153,20 +153,38 @@ public:
 	}
 };
 
+
+ll st_gcd(ll a, ll b) {
+	return gcd(a, b);
+}
+
+ll st_min(ll a, ll b) {
+	return min(a, b);
+}
+
+ll st_max(ll a, ll b) {
+	return max(a, b);
+}
+
+/// <summary>
+/// index starts 0
+/// </summary>
 class SparseTable {
-	vector< vector<ll> > table;
-	ll(*func)(ll, ll);
 
 public:
+	vector< vector<ll> > table;
+	ll(*func)(ll, ll);
+	ll deep = 0;
+
 	SparseTable(vector<ll> vec, ll(*f)(ll, ll)) {
 		this->func = f;
 		ll s = vec.size();
-		ll col = floor(log2(s));
-		table.resize(col + 1);
+		deep = floor(log2(s));
+		table.resize(deep + 1);
 		table[0].resize(s);
 		rep(i, s)
 			table[0][i] = vec[i];
-		rrep(k, col) {
+		rrep(k, deep) {
 			ll g = pow(2, k - 1);
 			table[k].resize(s);
 			rep(i, s - (g * 2 - 1)) {
@@ -175,20 +193,30 @@ public:
 		}
 	}
 
-	bool query(ll l, ll r) {
-		ll g = floor(log2(r));
-		ll ret = this->func(table[g][l], table[g][l + r - pow(2, g)]);
-		return (ret > 1);
+	/// <summary>
+	/// index starts 0
+	/// </summary>
+	/// <param name="st">index of start pos</param>
+	/// <param name="size">size of query</param>
+	/// <returns></returns>
+	ll query(ll st, ll size) {
+		ll g = floor(log2(size));
+		ll ret = this->func(table[g][st], table[g][st + size - pow(2, g)]);
+		return ret;
 	}
+
 };
 
+/// <summary>
+/// index starts 1
+/// </summary>
 class BITree {
 public:
 	vector<ll> v;
 	int sz;
 
 	BITree(int n) {
-		v.resize(n + 1,0);
+		v.resize(n + 1, 0);
 		sz = n;
 	}
 
@@ -212,63 +240,90 @@ public:
 	}
 };
 
+ll ones[11] = {
+1,	//0
+11,
+111,
+1111,
+11111,
+111111,
+1111111,
+11111111,
+111111111,
+1111111111,
+11111111111 };
 
-int main() {
-	ll n; cin >> n;
-	int c;
-	vector<ll> b(n,0),a(n,0);
-	map<int, int> mp,mpi;
-	//1,2,3,4,5 < a
-	//1,4,3,2,5 < b
-	rep(i, n) {
-		cin >> c;
-		mp[c] = i;
-		mpi[i] = c;
-	}
-	rep(i, n) {
-		cin >> c;
-		b[i] = mp[c];
-	}
-	//0,1,2,3,4 < a
-	//0,3,2,1,4 < b
-	ll dist = 0;
-	segmentTree st(n+1);
-	mp.clear();
-	rep(i, n) {
-		st.updateNode(b[i], 1);
-		int les = st.query(0, b[i]);
-		dist += (i - les+1);
-		mp[b[i]] = (i - les + 1);
-	}
-	if (dist % 2 != 0) {
-		cout << "-1\n";
+ll getOnes(int dig) {
+	if (dig >= 0)
+		return ones[dig];
+	else
 		return 0;
+}
+
+ll pow10s[] = {
+1,10,100,1000,10000,100000,1000000,10000000,100000000,1000000000,10000000000,10000000000 };
+;
+
+ll nearString(ll target, int a, int b, int len, ll bv) {
+	//make sure a>b
+	ll init_value = a * pow10s[len - 1];
+	if (a < b) swap(a, b);
+	rep(i, pow(2, len - 1)) {
+		ll r = init_value;
+		rep(j, len - 1) {
+			if ((i >> j) & 0x01)
+				r += a * pow10s[j];
+			else
+				r += b * pow10s[j];
+		}
+		if (r >= bv)
+			return bv;
+		if (r >= target)
+			return r;
+	}
+	return pow10s[len];
+}
+
+void solve() {
+	ll n, k;
+	cin >> n >> k;
+	string m = to_string(n);
+	int fd = m[0] - '0';
+	int dig = m.size();
+	ll val = 1;
+	if (k == 1) {
+		val = getOnes(dig - 1);
+		if (val * fd < n)
+			val = (fd + 1) * val;
+		else
+			val = val * fd;
+		cout << val << "\n";
+		return;
+	}
+	else if (k == 2) {
+		if (dig == 1) {
+			cout << m << "\n";
+			return;
+		}
+		ll ret = pow10s[dig];
+		for (int sd = 0; sd <= 9; sd++) {
+			ll f = nearString(n, fd, sd, dig,ret);
+			if (f < ret)
+				ret = f;
+		}
+		cout << ret << "\n";
 	}
 	else {
-		dist /= 2;
-		rep(i, n) {
-			if (mp[i] >= dist) {
-				mp[i] -= dist;
-				dist = 0;
-				break;
-			}
-			else {
-				dist -= mp[i];
-				mp[i] = 0;
-			}
-		}
-		segmentTree st1(n);
-		rep(i, n) st1.setNode(i, 1);
-		st1.calculateTree();
-		rep(i, n) {
-			int ind = st1.querySumIndex(0, 1 + mp[i]);
-			st1.updateNode(ind, 0);
-			b[ind] = i;
-		}
-
-		rep(i, n) {
-			cout << mpi[b[i]] << " ";
-		}
+		cout << "0";
 	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--)
+		solve();
 	return 0;
 }
